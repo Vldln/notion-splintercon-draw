bed92fa56df7841f4676aa0d5ba30565dbb697c0
Vldln <joker5916@gmail.com>
2024-08-26 18:02:04 +0400

    fix: fetch data naming

--- server/api/database.get.js
+++ server/api/database.get.js
@@ -3,7 +3,7 @@ import { Client } from "@notionhq/client";
 const notion = new Client({ auth: process.env.VUE_APP_NOTION_API_KEY });
 const image_database_id = process.env.VUE_APP_NOTION_DATABASE_ID;
 
-async function getImages() {
+async function getData() {
   const data = await notion.databases.query({
     database_id: image_database_id,
   });
@@ -12,13 +12,13 @@ async function getImages() {
 
 export default defineEventHandler(async () => {
   try {
-    const images = await getImages();
-    return images;
+    const data = await getData();
+    return data;
   } catch (error) {
-    console.error('Error fetching images from Notion:', error);
+    console.error("Error fetching data from Notion:", error);
     throw createError({
       statusCode: 500,
-      statusMessage: 'Failed to fetch images from Notion',
+      statusMessage: "Failed to fetch data from Notion",
     });
   }
 });
